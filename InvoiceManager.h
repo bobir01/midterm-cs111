7d1323d315b6c1f31a44e0e82cb1ce0f7ebf91fa
bobir01 <bobirtemka123@gmail.com>
2022-04-01 01:11:33 +0500

    initial steps

---
+++ InvoiceManager.h
@@ -0,0 +1,14 @@
+//
+// Created by bobir on 01/04/2022.
+//
+
+#ifndef MIDTERM_CS111_INVOICEMANAGER_H
+#define MIDTERM_CS111_INVOICEMANAGER_H
+
+
+class InvoiceManager {
+
+};
+
+
+#endif //MIDTERM_CS111_INVOICEMANAGER_H
