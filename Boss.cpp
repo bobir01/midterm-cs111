4df5c76e7ef0a7025acdbcb1387f3ed126cbb965
bobir01 <68824641+bobir01@users.noreply.github.com>
2022-04-01 04:27:14 +0500

    Boss.cpp

--- Boss.cpp
+++ Boss.cpp
@@ -1,5 +1,72 @@
-//
-// Created by bobir on 01/04/2022.
-//
 
 #include "Boss.h"
+#include <random>
+#include <cstdlib>
+#include <time.h>
+
+Boss::Boss(str fname, str lname, str company, str released_day, unsigned int number_employers, str id = "")
+{
+    this->fname = fname;
+    this->lname =lname;
+    this-> company = company ;
+    this-> found_day = released_day;
+    this -> num_employer = number_employers;
+}
+
+Boss::~Boss(){}
+
+void Boss::set_fname(str fname){
+    fname[0]=toupper(fname[0]); // to avoid from name being small case letter 
+    this -> fname = fname ;
+
+}
+
+str Boss:: get_fname() const {
+    return fname;
+}
+
+void Boss::set_lname(str lname){
+
+    lname [0]=toupper(lname[0]);
+    this ->lname=lname;
+}
+
+str Boss::get_lname() const{
+    return lname;
+}
+
+void Boss::set_released_day(str released_day){
+    this -> found_day=released_day;
+}
+
+str Boss::get_released_day(){
+    return found_day;
+};
+
+void Boss::set_company(str company_name){
+    this -> company = company_name;
+}
+
+str Boss:: get_company(){
+    return company;
+}
+
+void Boss::set_num_employer(unsigned int number_employers){
+    this -> num_employer= number_employers;
+}
+
+unsigned int Boss::get_num_employers(){
+    return num_employer;
+}
+
+str Boss::create_id()  {
+    srand(time(0));
+    str UUID= to_string(rand());
+    this -> id = get_company()[0] + UUID;
+    return  id;
+     
+}
+
+str Boss::get_id(){
+    return id;
+}
--- Boss.h
+++ Boss.h
@@ -13,19 +13,20 @@ typedef std::string str;
 class Boss
 {
 public:
-    Boss(str fname, str lname, str company, str released_day, unsigned int num_employee, str id);
+    Boss(str fname, str lname, str company, str released_day, unsigned int num_employers, str id="");
     ~Boss();
-    void set_fname(str fname);
-    str get_fname();
+    str get_data_of_boss();
+    void set_fname(str fname) ;
+    str get_fname() const ;
     void set_lname(str lname);
-    str get_lname();
+    str get_lname() const;
     void set_released_day(str released_day);
     str get_released_day();
     void set_company(str company_name);
     str get_company();
     void set_num_employer(unsigned int num_employers);
     unsigned int get_num_employers();
-    str create_id();
+    virtual str create_id()=0;
     str get_id();
 
 private:
@@ -34,7 +35,7 @@ private:
     str company;
     str found_day;
     str id;
-    unsigned int num_employer;
+    unsigned int num_employer = 0;
 };
 
 #endif // MIDTERM_CS111_BOSS_H
